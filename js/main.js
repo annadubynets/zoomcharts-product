e9c3e531a0d85dcd3db8c2dd15d319f51e40a74d
Ivan Dubynets <ivan.dubynets@gmail.com>
2022-07-19 09:12:20 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -3022,7 +3022,7 @@
 $('.objects-carousel').slick({
     dots: false,
     arrows: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
@@ -3030,9 +3030,7 @@ $('.objects-carousel').slick({
             breakpoint: 1024,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                arrows: true
+                slidesToScroll: 2
             }
         },
         {
