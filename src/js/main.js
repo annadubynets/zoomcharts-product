b793a1dd48316ff5bd681e2581ac300f1ee23d88
Ivan Dubynets <ivan.dubynets@gmail.com>
2022-08-02 19:19:55 +0200

    create new page Report

--- src/js/main.js
+++ src/js/main.js
@@ -64,6 +64,37 @@ $('.bundle-carousel').slick({
     ]
 });
 
+$('.customer-carousel').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 9,
+    slidesToScroll: 9,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 5,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+            }
+        }
+    ]
+});
+
 function setupGetItNowBanner() {
     const banner = document.querySelector('.get-it-now-banner');
     const targetSection = document.querySelector('.get-it-now-section');
