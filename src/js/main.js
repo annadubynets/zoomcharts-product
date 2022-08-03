ab2e605595c3405dab81d75525c4d936e923751a
Ivan Dubynets <ivan.dubynets@gmail.com>
2022-08-03 22:58:29 +0200

    prepared slick to the trick

--- src/js/main.js
+++ src/js/main.js
@@ -40,26 +40,26 @@ $('.bundle-carousel').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 9,
-    slidesToScroll: 9,
+    slidesToScroll: 1,
     responsive: [{
             breakpoint: 1024,
             settings: {
                 slidesToShow: 5,
-                slidesToScroll: 5,
+                slidesToScroll: 1,
             }
         },
         {
             breakpoint: 600,
             settings: {
                 slidesToShow: 4,
-                slidesToScroll: 4
+                slidesToScroll: 1
             }
         },
         {
             breakpoint: 480,
             settings: {
                 slidesToShow: 4,
-                slidesToScroll: 4
+                slidesToScroll: 1
             }
         }
     ]
